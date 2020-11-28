23c1534f4967f0e65800a50e1a5eef6c81075d65
Loren Peralta <stinpc@hotmail.com>
2020-11-27 22:04:17 -0500

    Editar y elimnar clientes

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MinerTrabajoFInal.Data;
 using MinerTrabajoFInal.Models;
@@ -23,25 +24,80 @@ namespace MinerTrabajoFInal.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var lista = _context.Clientes.ToList();
+            return View(lista);
         }
 
         [HttpPost]
-        public IActionResult Crear(Cliente objClientes)
+        public IActionResult Registrar(Cliente objCliente)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(objClientes);
+                _context.Add(objCliente);
                 _context.SaveChanges();
                 return RedirectToAction("Confirmacion");
 
             }
-            return View("index",objClientes);
+            return View("Crear", objCliente);
         }
 
         public IActionResult Confirmacion()
         {
             return View();
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(int id, [Bind("id,razonsocial,dni,direccion,telefono,correo")] Cliente objCliente)
+        {   
+            if (id != objCliente.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(objCliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(objCliente);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var contacto = _context.Clientes.Find(id);
+            _context.Clientes.Remove(contacto);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
