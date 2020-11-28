cd8991d0ccbd6b7c662c11459d5d07a03095893e
RONYBCS <ronycondorisanchez@gmail.com>
2020-11-28 11:00:30 -0500

    Formulario_Terminado

---
+++ Models/Resultado.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinerTrabajoFInal.Models
+{
+    [Table("recepcion")]    
+    public class Resultado
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id  { get; set; }
+        [Column("idcliente")]
+        public int idcliente { get; set; }
+        [Column("numerorecpcion")]
+        public int numerorecpcion { get; set; }
+        [Column("tipo_analisis")]
+        public string tipo_analisis  { get; set; }        
+        [Column("muestra")]
+        public string muestra  { get; set; }
+        [Column("precio")]
+        public double precio  { get; set; }
+        [Column("cantidad")]
+        public int cantidad  { get; set; }
+        [Column("valor")]
+        public string valor  { get; set; } 
+    }
+}
