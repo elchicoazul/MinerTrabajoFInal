efd2f6a48098ef2c503cc0603ae0a77bfbed4f5e
RONYBCS <ronycondorisanchez@gmail.com>
2020-12-05 08:39:08 -0500

    Mostrando Resultado

--- Controllers/ResultadoController.cs
+++ Controllers/ResultadoController.cs
@@ -25,6 +25,46 @@ namespace MinerTrabajoFInal.Controllers
             var resultado=_context.Resultado.ToList();
             return View(resultado);
         }
+        [HttpPost]
+        public IActionResult Buscar (Resultado objResultado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.search(objCliente);
+                _context.SaveChanges();
+                return RedirectToAction("Confirmacion");
+
+            }
+            return View("Crear", objCliente);
+        }
  
+    }
+
+          
+
+    public void busqueda(View view) {
+        //String rpta; LISTO  PARA USAS  CON  LA  CLASE
+
+        AdminSQLiteOpenHelper db = new AdminSQLiteOpenHelper(this, "SoyAdmin!", null, 1);
+        SQLiteDatabase dbse = db.getWritableDatabase();
+        String a1 = codi.getText().toString();
+        if(!a1.isEmpty()){
+          //  ClaseProducto pro = new ClaseProducto();
+           // rpta=pro.select_cod(a1);
+            Cursor fila = dbse.rawQuery("select * from productos where codigo ="+a1, null);
+            if(fila.moveToFirst()){
+                //db.execSQL("create table productos(codigo int  primary key , nombre text, categoria int, descripcion text, precio real,stock int)");
+                codi.setText(fila.getString(0));
+                Nom_pro.setText(fila.getString(1));
+                cat.setText(fila.getString(2));
+                descr.setText(fila.getString(3));
+                price.setText(fila.getString(4));
+                st.setText(fila.getString(5));}
+            else{
+                Toast.makeText(this, "Consulta  sin  resultados", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Llenar Codigo", Toast.LENGTH_SHORT).show();
+        }
     }
 }
