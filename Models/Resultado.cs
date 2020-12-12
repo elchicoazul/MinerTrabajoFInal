fb5b31e1d6dea09c8d9fb05378be45cbaccab7c0
Jorge chipana <jfchipanac@gmail.com>
2020-12-12 07:44:03 -0500

    modificacion de formulario

--- Models/Resultado.cs
+++ Models/Resultado.cs
@@ -13,15 +13,24 @@ namespace MinerTrabajoFInal.Models
         [Column("idcliente")]
         [Display(Name="Nombre CLiente")]
         public int idcliente { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese un numero de recepcion")] 
         [Column("numerorecpcion")]
+        [Display(Name="Numero de Recepcion")]
         public int numerorecpcion { get; set; }
         [Column("tipo_analisis")]
-        public string tipo_analisis  { get; set; }        
+        [Display(Name="Tipo de Analisis")]
+        public string tipo_analisis  { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese una muestra")]         
         [Column("muestra")]
+        [Display(Name="Muestra")]
         public string muestra  { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese un precio")] 
         [Column("precio")]
+        [Display(Name="Precio")]
         public double precio  { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese una cantidad")] 
         [Column("cantidad")]
+        [Display(Name="Cantidad")]
         public int cantidad  { get; set; }
         [Column("valor")]
         public string valor  { get; set; } 
