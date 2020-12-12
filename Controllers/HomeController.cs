c76bb04be2cbda960825a1a6d9ccd2e8c4d01267
lorenperalta <stinpc@hotmail.com>
2020-12-11 21:07:57 -0500

    Acerca de nosotros

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace MinerTrabajoFInal.Controllers
             return View();
         }
 
+        public IActionResult Acerca()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
